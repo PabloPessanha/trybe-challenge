6656478b7d3421686f7e6599c5308796a9bb3537
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-26 01:02:11 -0300

    backend: update POST method

--- src/controllers/developerController.js
+++ src/controllers/developerController.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
+const { User } = require('../models');
 
 const developer = Router();
 
 developer.post('/developer', async (req, res) => {
-  const { name, landline, phone, CEP, skills } = req.body;
+  const { name, telphone_number, celphone_number } = req.body;
+  const user = await User.create({ name, telphone_number, celphone_number });
 
-  return res.status(200).json();
+  return res.status(200).json(user);
 });
 
 module.exports = developer;
