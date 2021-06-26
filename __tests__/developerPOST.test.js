3939a6f8e3905f328b107c1672ea6ff76230070b
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-26 01:06:49 -0300

    backend: realocated utils mocks to test folder; chage POST file name

--- __tests__/developerPOST.test.js
+++ __tests__/developerPOST.test.js
@@ -1,6 +1,6 @@
 const frisby = require('frisby');
 const shell = require('shelljs');
-const devs = require('../utils/devs');
+const devs = require('./utils/devs');
 require('dotenv').config();
 
 const URL = process.env.USER_URL || 'http://localhost:3000/developer';
@@ -17,21 +17,17 @@ describe('Verifica o método POST da URL "/developer"', () => {
   });
 
   it('Verifica se o  status é 200 quando todos os campos são passados corretamente', async () => {
-    let response = await frisby.post(URL, devs[0]);
-    const firstPerson = JSON.parse(response.body);
+    const response = await frisby.post(URL, devs[0]);
 
     expect(response.status).toBe(200);
-    expect(firstPerson.id).toBe(1);
-    expect(firstPerson.name).toBe('Pablo');
-
-    response = await frisby.post(URL, devs[1]);
-    const secondPerson = JSON.parse(response.body);
-    expect(response.status).toBe(200);
-    expect(secondPerson.id).toBe(2);
-    expect(secondPerson.name).toBe('Renato');
   });
 
-  it('Verifica se o retorno de dado é correto', async () => {
+  it('Verifica se o retorno é o objeto que acabou de ser armazenado ao BD', async () => {
+    const { json } = await frisby.post(URL, devs[0]);
 
+    expect(json.id).toBe(1);
+    expect(json.name).toBe('Pablo');
+    expect(json.telphone_number).toBe('24 3512-2351');
+    expect(json.celphone_number).toBe('21 95512-3321');
   });
 });
