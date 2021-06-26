7f2530ef50256a22c2ac379f01316d578c1e3431
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-25 21:53:12 -0300

    tests: add mock infos

---
+++ utils/devs.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    name: 'Pablo',
+    landline: '24 3512-2351',
+    phone: '21 95512-3321',
+    CEP: '25942652',
+    skills: ['Javascript', 'Python', 'Ruby'],
+  },
+  {
+    name: 'Renato',
+    phone: '65 95512-3244',
+    CEP: '41321205',
+    skills: ['Javascript', 'Python', 'Java'],
+  },
+  {
+    name: 'Carlos',
+    phone: '74 95512-5123',
+    CEP: '25942652',
+    skills: ['Javascript', 'C#', 'Go'],
+  },
+];
