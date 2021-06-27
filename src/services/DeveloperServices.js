436997f537f415f1a45475a62350ed35b970570a
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-27 00:46:58 -0300

    backend: add cep validation

--- src/services/DeveloperServices.js
+++ src/services/DeveloperServices.js
@@ -1,15 +1,18 @@
 const Joi = require('joi');
+const cepPromise = require('cep-promise');
 
 class DeveloperServices {
   constructor(obj) {
     this.name = obj.name;
-    this.telphone_number = obj.telphone_number;
-    this.celphone_number = obj.celphone_number;
-    this.cep = obj.cep;
+    this.telphone_number = obj.telphone_number?.toString();
+    this.celphone_number = obj.celphone_number?.toString();
+    this.cep = obj.cep?.toString();
     this.skills = obj.skills;
 
     this.validateName();
     this.validateTelephone();
+    this.validateCelphone();
+    this.validateCep();
   }
 
   async validateName() {
@@ -20,18 +23,27 @@ class DeveloperServices {
   }
 
   async validateTelephone() {
-    const validator = Joi.string().length(10).pattern(/^\d+$/).required();
-    await validator.validateAsync(this.telphone_number.toString())
+    const validator = Joi.string().length(10).pattern(/^\d+$/);
+    await validator.validateAsync(this.telphone_number)
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
   async validateCelphone() {
     const validator = Joi.string().length(11).pattern(/^\d+$/).required();
-    await validator.validateAsync(this.celphone_number.toString())
+    await validator.validateAsync(this.celphone_number)
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
+
+  async validateCep() {
+    await cepPromise(this.cep)
+      .then(console.log)
+      .catch((_err) => { throw new Error('Cep inválido') });
+  }
 }
 
-module.exports = new DeveloperServices();
+const teste = new DeveloperServices({ name: 'Pablo', celphone_number: 21985199407, cep: '03047000' });
+console.log(teste);
+
+// module.exports = new DeveloperServices();
