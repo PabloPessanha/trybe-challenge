99e1825a73431d8db7680f40f32cc8a6a9adbe25
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-27 15:24:57 -0300

    backend: add creation user, address and user language logic with sequelize

--- src/controllers/DeveloperController.js
+++ src/controllers/DeveloperController.js
@@ -1,13 +1,22 @@
-// const { User } = require('../models');
+const { Op } = require('sequelize');
+const { User, Address, Language } = require('../models');
 const DeveloperServices = require('../services/DeveloperServices');
 
 class DeveloperController {
   async store(req, res) {
     try {
-      const adress = await DeveloperServices(req.body);
-      const user = { ...req.body, adress };
+      const { rua, bairro, cidade, uf, cep } = await DeveloperServices(req.body);
+      const { name, telphone_number = null, celphone_number, languages } = req.body;
 
-      return res.status(200).json(user);
+      const { id, ...userInfos } = await User.create({ name, telphone_number, celphone_number });
+      const endereco = await Address.create({ id, rua, bairro, cidade, uf, cep });
+
+      const languagesId = await Promise.all(languages.map(async (skill) => {
+        const { id: languageId } = Language.findOne({ where: { language: { [Op.like]: skill } } });
+        return languageId;
+      }));
+
+      return res.status(200).json({ ...userInfos, endereco, languages, languagesId });
     } catch ({ message }) {
       if (message) return res.status(400).json({ message });
       return res.status(500).json({ message: 'internal error' });
