b48e6961d1d8936cb3106420091aa4edf31e6875
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-27 13:56:33 -0300

    lint: update rules

--- src/controllers/DeveloperController.js
+++ src/controllers/DeveloperController.js
@@ -1,11 +1,17 @@
-const { User } = require('../models');
+// const { User } = require('../models');
+const DeveloperServices = require('../services/DeveloperServices');
 
 class DeveloperController {
   async store(req, res) {
-    const { name, telphone_number, celphone_number } = req.body;
-    const user = await User.create({ name, telphone_number, celphone_number });
+    try {
+      const adress = await DeveloperServices(req.body);
+      const user = { ...req.body, adress };
 
-    return res.status(200).json(user);
+      return res.status(200).json(user);
+    } catch ({ message }) {
+      if (message) return res.status(400).json({ message });
+      return res.status(500).json({ message: 'internal error' });
+    }
   }
 }
 
