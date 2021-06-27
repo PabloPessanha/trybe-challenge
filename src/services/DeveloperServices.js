923e96a103357136b43a27e55f33bf442ed817f2
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-27 12:53:27 -0300

    backend: change DeveloperServices structure; add validate fields

--- src/services/DeveloperServices.js
+++ src/services/DeveloperServices.js
@@ -1,49 +1,43 @@
 const Joi = require('joi');
 const cepPromise = require('cep-promise');
 
-class DeveloperServices {
-  constructor(obj) {
-    this.name = obj.name;
-    this.telphone_number = obj.telphone_number?.toString();
-    this.celphone_number = obj.celphone_number?.toString();
-    this.cep = obj.cep?.toString();
-    this.skills = obj.skills;
-
-    this.validateName();
-    this.validateTelephone();
-    this.validateCelphone();
-    this.validateCep();
+module.exports = (obj) =>  {
+  async function validateFields() {
+    const validator = Joi.object({ 
+      name: Joi.required(), 
+      celphone_number: Joi.required(), 
+      cep: Joi.required(),
+      skills: Joi.required()
+    });
+    await validator.validateAsync(this.obj)
+      .then()
+      .catch(({ message }) => { throw new Error(message); });
   }
 
-  async validateName() {
+  async function validateName() {
     const validator = Joi.string().min(5).required();
     await validator.validateAsync(this.name)
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
-  async validateTelephone() {
+  async function validateTelephone() {
     const validator = Joi.string().length(10).pattern(/^\d+$/);
     await validator.validateAsync(this.telphone_number)
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
-  async validateCelphone() {
+  async function validateCelphone() {
     const validator = Joi.string().length(11).pattern(/^\d+$/).required();
     await validator.validateAsync(this.celphone_number)
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
-  async validateCep() {
+  async function validateCep() {
     await cepPromise(this.cep)
-      .then(console.log)
+      .then((adress) => this.adress = adress)
       .catch((_err) => { throw new Error('Cep inválido') });
   }
-}
-
-const teste = new DeveloperServices({ name: 'Pablo', celphone_number: 21985199407, cep: '03047000' });
-console.log(teste);
-
-// module.exports = new DeveloperServices();
+};
