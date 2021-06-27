75f49f446ba7c6c6158640a803e2484058e997f4
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-27 13:36:38 -0300

    backend: DeveloperServices validations done

--- src/services/DeveloperServices.js
+++ src/services/DeveloperServices.js
@@ -1,43 +1,62 @@
 const Joi = require('joi');
 const cepPromise = require('cep-promise');
 
-module.exports = (obj) =>  {
+async function DeveloperServices(obj) {
   async function validateFields() {
     const validator = Joi.object({ 
       name: Joi.required(), 
       celphone_number: Joi.required(), 
+      skills: Joi.required(),
       cep: Joi.required(),
-      skills: Joi.required()
     });
-    await validator.validateAsync(this.obj)
+    await validator.validateAsync(obj)
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
   async function validateName() {
     const validator = Joi.string().min(5).required();
-    await validator.validateAsync(this.name)
+    await validator.validateAsync(obj.name)
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
   async function validateTelephone() {
     const validator = Joi.string().length(10).pattern(/^\d+$/);
-    await validator.validateAsync(this.telphone_number)
+    await validator.validateAsync(obj.telphone_number?.toString())
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
   async function validateCelphone() {
     const validator = Joi.string().length(11).pattern(/^\d+$/).required();
-    await validator.validateAsync(this.celphone_number)
+    await validator.validateAsync(obj.celphone_number?.toString())
       .then()
       .catch(({ message }) => { throw new Error(message); });
   }
 
   async function validateCep() {
-    await cepPromise(this.cep)
-      .then((adress) => this.adress = adress)
+    return await cepPromise(obj.cep)
+      .then((adress) => adress)
       .catch((_err) => { throw new Error('Cep inválido') });
   }
+
+  async function validateSkills() {
+    const validSkills = ['Javascript', 'Java', 'Go', 'Python', 'C#', 'Elixir']
+    const validator = Joi.array().items(
+      Joi.string().valid(...validSkills).insensitive().required(),
+      Joi.string().valid(...validSkills).insensitive().required())
+    await validator.validateAsync(obj.skills)
+      .then()
+      .catch(({ message }) => { throw new Error(message); })
+  }
+
+  await validateFields();
+  await validateName();
+  await validateTelephone();
+  await validateCelphone();
+  await validateSkills();
+  return await validateCep();
 };
+
+module.exports = DeveloperServices;
