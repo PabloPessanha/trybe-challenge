ec645faf1f34dcbd65aba11914021c50cdb5be41
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-28 18:30:47 -0300

    backend: update models values; retrive informations for response return

--- src/controllers/DeveloperController.js
+++ src/controllers/DeveloperController.js
@@ -1,22 +1,22 @@
 const { Op } = require('sequelize');
-const { User, Address, Language } = require('../models');
-const DeveloperServices = require('../services/DeveloperServices');
+const { user: User, address: Address, language: Language } = require('../models');
+const { getCepData } = require('../services/DeveloperServices');
 
 class DeveloperController {
   async store(req, res) {
     try {
-      const { rua, bairro, cidade, uf, cep } = await DeveloperServices(req.body);
-      const { name, telphone_number = null, celphone_number, languages } = req.body;
+      const { rua, bairro, cidade, uf, cep } = await getCepData(req.body.cep);
+      const { name, telphone_number = null, celphone_number, skills } = req.body;
 
-      const { id, ...userInfos } = await User.create({ name, telphone_number, celphone_number });
-      const endereco = await Address.create({ id, rua, bairro, cidade, uf, cep });
+      const { id, dataValues } = await User.create({ name, telphone_number, celphone_number });
+      const { dataValues: { user_id, ...endereco } } = await Address.create({ user_id: id, rua, bairro, cidade, uf, cep });
 
-      const languagesId = await Promise.all(languages.map(async (skill) => {
-        const { id: languageId } = Language.findOne({ where: { language: { [Op.like]: skill } } });
-        return languageId;
+      const languagesId = await Promise.all(skills.map(async (skill) => {
+        const lang = await Language.findOne({ where: { language: { [Op.iLike]: skill } } });
+        return lang;
       }));
 
-      return res.status(200).json({ ...userInfos, endereco, languages, languagesId });
+      return res.status(200).json({ ...dataValues, endereco, languagesId });
     } catch ({ message }) {
       if (message) return res.status(400).json({ message });
       return res.status(500).json({ message: 'internal error' });
