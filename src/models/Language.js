9e108eafc0297bad55c76e89939d2b29313f4317
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-28 18:19:18 -0300

    database: add primary key on id association language model

--- src/models/Language.js
+++ src/models/Language.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const Language = sequelize.define('language', {
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
     language: DataTypes.STRING,
   }, { timestamps: false, tableName: 'languages' });
 
