ce925ddbe670fae88f62000de1435c4b396de189
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-28 18:16:49 -0300

    backend: add middleware validations on developer post route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const DeveloperController = require('./controllers/DeveloperController');
+const validateAll = require('./middlewares/validateAll');
 
 const developer = new Router();
 
-developer.post('/developer', DeveloperController.store);
+developer.post('/developer', validateAll, DeveloperController.store);
 
 module.exports = developer;
