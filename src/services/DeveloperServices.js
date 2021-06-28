f8aeb3197510d3e1334640b74b76b88ad52b0845
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-28 20:35:48 -0300

    backend: create 'addUserSkils' function to make an association to languages user on database

--- src/services/DeveloperServices.js
+++ src/services/DeveloperServices.js
@@ -1,5 +1,6 @@
 const cepPromise = require('cep-promise');
-const { user: User, address: Address } = require('../models');
+const { Op } = require('sequelize');
+const { user: User, address: Address, language: Language, userLanguages } = require('../models');
 
 async function getCepData(cep) {
   return cepPromise(cep)
@@ -12,22 +13,35 @@ async function getCepData(cep) {
     .catch((_err) => { throw new Error('Cep inválido'); });
 }
 
-async function createUser({ name, telphone_number = null, celphone_number, cep }) {
+async function addUserSkills(skills, user_id) {
+  const languages = await Promise.all(skills.map(async (skill) => {
+    const { dataValues } = await Language.findOne({ where: { language: { [Op.iLike]: skill } } });
+    const { id: language_id, language } = dataValues;
+
+    await userLanguages.create({ language_id, user_id });
+    return language;
+  }));
+
+  return languages;
+}
+
+async function createUser({ name, telphone_number = null, celphone_number, cep, skills }) {
   const { rua, bairro, cidade, uf } = await getCepData(cep);
 
   const { id } = await User.create({ name, telphone_number, celphone_number });
   const { dataValues } = await Address.create({ user_id: id, rua, bairro, cidade, uf, cep });
 
   const { user_id, ...endereco } = dataValues;
+  const especialidades = await addUserSkills(skills, user_id);
   return {
     nome: name,
     telefone: telphone_number,
     celular: celphone_number,
     endereco,
+    especialidades,
   };
 }
 
 module.exports = {
-  getCepData,
   createUser,
 };
