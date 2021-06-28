74a5914f08b6c3eb9dc6755cbbb74dbee9b69185
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-28 20:15:17 -0300

    refac: pass all create logic user to services module

--- src/controllers/DeveloperController.js
+++ src/controllers/DeveloperController.js
@@ -1,22 +1,18 @@
 const { Op } = require('sequelize');
-const { user: User, address: Address, language: Language } = require('../models');
-const { getCepData } = require('../services/DeveloperServices');
+const { language: Language } = require('../models');
+const { createUser } = require('../services/DeveloperServices');
 
 class DeveloperController {
   async store(req, res) {
     try {
-      const { rua, bairro, cidade, uf, cep } = await getCepData(req.body.cep);
-      const { name, telphone_number = null, celphone_number, skills } = req.body;
+      const userData = await createUser(req.body);
 
-      const { id, dataValues } = await User.create({ name, telphone_number, celphone_number });
-      const { dataValues: { user_id, ...endereco } } = await Address.create({ user_id: id, rua, bairro, cidade, uf, cep });
-
-      const languagesId = await Promise.all(skills.map(async (skill) => {
-        const lang = await Language.findOne({ where: { language: { [Op.iLike]: skill } } });
-        return lang;
+      const languagesId = await Promise.all(req.body.skills.map(async (skill) => {
+        const { dataValues: { language } } = await Language.findOne({ where: { language: { [Op.iLike]: skill } } });
+        return language;
       }));
 
-      return res.status(200).json({ ...dataValues, endereco, languagesId });
+      return res.status(200).json({ ...userData, languagesId });
     } catch ({ message }) {
       if (message) return res.status(400).json({ message });
       return res.status(500).json({ message: 'internal error' });
--- src/services/DeveloperServices.js
+++ src/services/DeveloperServices.js
@@ -1,4 +1,5 @@
 const cepPromise = require('cep-promise');
+const { user: User, address: Address } = require('../models');
 
 async function getCepData(cep) {
   return cepPromise(cep)
@@ -11,6 +12,22 @@ async function getCepData(cep) {
     .catch((_err) => { throw new Error('Cep inválido'); });
 }
 
+async function createUser({ name, telphone_number = null, celphone_number, cep }) {
+  const { rua, bairro, cidade, uf } = await getCepData(cep);
+
+  const { id } = await User.create({ name, telphone_number, celphone_number });
+  const { dataValues } = await Address.create({ user_id: id, rua, bairro, cidade, uf, cep });
+
+  const { user_id, ...endereco } = dataValues;
+  return {
+    nome: name,
+    telefone: telphone_number,
+    celular: celphone_number,
+    endereco,
+  };
+}
+
 module.exports = {
   getCepData,
+  createUser,
 };
