7391d03cbcc18c97683c0393240aab1efb793e1a
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-28 18:22:09 -0300

    backend: add 'next' param for keep going on the application

--- src/middlewares/validateAll.js
+++ src/middlewares/validateAll.js
@@ -4,13 +4,14 @@ const validateTelphone = require('./validateTelphone');
 const validateCelphone = require('./validateCelphone');
 const validateSkills = require('./validateSkills');
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
   try {
     await validateFields(req.body);
     await validateName(req.body.name);
     await validateCelphone(req.body.celphone_number);
     await validateTelphone(req.body.telphone_number);
     await validateSkills(req.body.skills);
+    next();
   } catch ({ message }) {
     return res.status(400).json({ message });
   }
