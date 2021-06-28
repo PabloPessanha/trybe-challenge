9e88581c094f3c4e868d2823b0aaba1bd65b62af
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-28 18:18:28 -0300

    database: fix tablename association; change attr association user; remove unnecessary id

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,16 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
   const Address = sequelize.define('address', {
-    user_id: DataTypes.INTEGER,
     rua: DataTypes.STRING,
     bairro: DataTypes.STRING,
     cidade: DataTypes.STRING,
     uf: DataTypes.STRING,
     cep: DataTypes.STRING,
-  }, { timestamps: false, tableName: 'adresses' });
+  }, { timestamps: false, tableName: 'addresses' });
 
   Address.associate = (models) => {
-    Address.belongsTo(models.Address, { as: 'user', foreignKey: 'user_id' });
+    Address.belongsTo(models.user, { foreignKey: 'user_id', as: 'user' });
   };
+  
+  Address.removeAttribute('id');
 
   return Address;
 };
