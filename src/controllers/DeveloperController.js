d6d540806e80ef0f4d96a74d60067c428a9d5d9b
Pablo Pessanha <pablopessanh@gmail.com>
2021-06-29 14:43:46 -0300

    backend: create method to get user by id

--- src/controllers/DeveloperController.js
+++ src/controllers/DeveloperController.js
@@ -1,3 +1,4 @@
+const { user: User, language: Language } = require('../models');
 const { createUser } = require('../services/DeveloperServices');
 
 class DeveloperController {
@@ -10,6 +11,19 @@ class DeveloperController {
       if (message) return res.status(400).json({ message });
       return res.status(500).json({ message: 'internal error' });
     }
+  }
+
+  async getById(req, res) {
+    const user = await User.findOne({ 
+      where: { id: req.params.id }, 
+      include: [{ model: Language, as: 'languages' }],
+      attributes: { exclude: ['id'] },
+    });
+    if (!user) return res.status(404).json({ message: 'User not found!' });
+    const { languages, ...userInfo } = user.dataValues;
+
+    const finalUser = { ...userInfo, skills: languages.map(({ language }) => language) };
+    return res.status(200).json(finalUser);
   }
 }
 
